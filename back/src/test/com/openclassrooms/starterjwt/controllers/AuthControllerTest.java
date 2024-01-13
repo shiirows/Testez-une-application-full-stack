25cf357e52f00cb4bbfffc2fd45f2fb28ee2ad24
Mazoyer <mazoyeralexandre@hotmail.fr>
2024-01-13 16:36:29 +0100

    conf properties backend

---
+++ back/src/test/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @Test
+    void authenticateUser() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+}
