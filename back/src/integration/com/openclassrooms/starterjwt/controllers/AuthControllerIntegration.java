658f036dd5ad4ef3260f86bee677891557b871b5
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-16 14:17:21 +0100

    add test integration for authcontroller

--- back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
+++ back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
@@ -1,26 +1,40 @@
 package com.openclassrooms.starterjwt.controllers;
-
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@Autowired
-private TestRestTemplate restTemplate;
 
-@Autowired
-private MockMvc mockMvc;
 
-@Autowired
-private JwtUtils jwtUtils;
 
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthControllerIntegration {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
     @Test
-    void authenticateUser() {
-     //   ce test permet de simuler une requête POST sur l'URL /api/auth/login avec un body contenant un username et un password qui retourne un token
+    void authenticateUser() throws Exception {
+        //   ce test permet de simuler une requête POST sur l'URL /api/auth/login avec un body contenant un username et un password qui retourne un token
         this.mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\": \"yoga", \"password\": \"yoga\"}"))
+                        .content("{ \"email\": \"yoga@studio.com\", \"password\": \"test!1234\" }")
                         .accept(MediaType.APPLICATION_JSON)
                         .with(csrf()))
                 .andExpect(status().isOk())
@@ -29,13 +43,13 @@ class AuthControllerIntegration {
     }
 
     @Test
-    void registerUser() {
+    void registerUser()  throws Exception {
         // ce test permet de simuler une requête POST sur l'URL /api/auth/register avec un body contenant un username, un email et un password qui retourne un token
         this.mockMvc.perform(post("/api/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\": \"yoga\", \"email\": \"yoga@test.com\", \"password\": \"yoga\"}"))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(csrf()))
+                        .content("{\"username\": \"yoga\", \"email\": \"yoga@test.com\", \"password\": \"yoga\"}")
+                .accept(MediaType.APPLICATION_JSON)
+                .with(csrf()))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
