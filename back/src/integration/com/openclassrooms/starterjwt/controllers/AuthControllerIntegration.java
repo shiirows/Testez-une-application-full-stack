764b5b32c5dd36146b7133ae1ea724a5850f23d2
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-16 15:58:26 +0100

    add test integration for authcontroller

--- back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
+++ back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
@@ -1,4 +1,5 @@
 package com.openclassrooms.starterjwt.controllers;
+
 import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,6 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-
-
 
 
 @AutoConfigureMockMvc
---
+++ back/src/integration/com/openclassrooms/starterjwt/controllers/TeacherControllerIntegration.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TeacherControllerIntegration {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
