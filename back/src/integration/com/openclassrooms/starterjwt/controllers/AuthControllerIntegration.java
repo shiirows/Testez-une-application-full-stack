cf4ff6be84630048ccb4c6f0edba27c05236ffbb
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-16 13:15:15 +0100

    add test integration for authcontroller

--- back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
+++ back/src/integration/com/openclassrooms/starterjwt/controllers/AuthControllerIntegration.java
@@ -17,9 +17,26 @@ class AuthControllerIntegration {
 
     @Test
     void authenticateUser() {
+     //   ce test permet de simuler une requête POST sur l'URL /api/auth/login avec un body contenant un username et un password qui retourne un token
+        this.mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"username\": \"yoga", \"password\": \"yoga\"}"))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andDo(print());
+
     }
 
     @Test
     void registerUser() {
+        // ce test permet de simuler une requête POST sur l'URL /api/auth/register avec un body contenant un username, un email et un password qui retourne un token
+        this.mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"username\": \"yoga\", \"email\": \"yoga@test.com\", \"password\": \"yoga\"}"))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
