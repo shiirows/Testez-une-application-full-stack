ace5f7420ceb7c9bf6f188cc08c7430b0efaf683
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-23 14:10:03 +0100

    add testing to jest for userService

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -6,17 +6,40 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController :HttpClientModule
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports:[ HttpClientModule ],
+      providers: [UserService]
     });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpClientModule);
   });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('should get user by ID', () => {
+ const userId = '1'
+ const mockUser = {id:userId, name: 'John Doe'};
+
+expect(service.getById(userId).subscribe(user => {
+  expect(user).toEqual(mockUser);
+  }))
+})
+
+it('should get user is not found', ()  => {
+  const userId = '10'
+  
+  expect(service.getById(userId).subscribe(user => {
+
+    expect(user).toBe(404);
+   
+  }))
+})
+
 });
