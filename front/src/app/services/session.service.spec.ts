356972f818ed7a92b1c3ef62980feb83726558e7
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-23 11:29:16 +0100

    update testing to jest for session

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -19,7 +19,7 @@ describe('SessionService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should log in', () => {
+  it('user login', () => {
     const user = {
       name: 'test',
       email: 'toto@gmail.com',
@@ -27,37 +27,44 @@ describe('SessionService', () => {
       admin: true
     };
 
-    it('should init  set isLogged to false', () => {
-      expect(service.isLogged).toBeFalsy();
-    });
-
-    it('should init set sessionInformation to undefined', () => {
-      expect(service.sessionInformation).toBeUndefined();
-    });
-  });
-
-  describe('$isLogged', () => {
-    it('should return an Observable for $isLogged()', () => {
-      expect(service.$isLogged()).toBeInstanceOf(Observable);
-    });
-  });
-
-  describe('logIn', () => {
-    it('should set sessionInformation and set isLogger to true on logIn', () => {
-      service.logIn(mockSessionUser);
-      expect(service.isLogged).toBeTruthy();
-      expect(service.sessionInformation).toEqual(mockSessionUser);
-    });
-  });
-
-  describe('logOut', () => {
-    it('should set sessionInformation to undefined and set isLogger to false on logOut', () => {
-      service.logOut();
-      expect(service.isLogged).toBeFalsy();
-      expect(service.sessionInformation).toBeUndefined();
-    });
-
 
 });
+
+describe('user is Logged', () => {
+  it('should return an Observable for $isLogged()', () => {
+    expect(service.$isLogged()).toBeInstanceOf(Observable);
+  });
+});
+
+
+it('should init set user login to false', () => {
+  expect(service.isLogged).toBeFalsy();
+});
+
+it('should init set sessionInformation to undefined', () => {
+  expect(service.sessionInformation).toBeUndefined();
+});
+
+
+describe('logIn', () => {
+  it('should set sessionInformation and set user Logger to true on logIn', () => {
+    service.logIn(mockSessionUser);
+    expect(service.isLogged).toBeTruthy();
+    expect(service.sessionInformation).toEqual(mockSessionUser);
+  });
+});
+
+describe('logOut', () => {
+  it('should set sessionInformation to undefined and set user Logger to false on logOut', () => {
+    service.logOut();
+    expect(service.isLogged).toBeFalsy();
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+
+})
+
+
+
 
 });
