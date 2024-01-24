387f0723e655fbb3bd537931fa2487baa5cc7b83
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-24 13:30:25 +0100

    add test user session

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
@@ -6,15 +7,16 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
-  let httpTestingController :HttpClientModule
+  let httpTesting:HttpTestingController
+  const path = 'api/user';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[ HttpClientModule ],
+      imports:[ HttpClientTestingModule ],
       providers: [UserService]
     });
     service = TestBed.inject(UserService);
-    httpTestingController = TestBed.inject(HttpClientModule);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
 
@@ -22,24 +24,50 @@ describe('UserService', () => {
     expect(service).toBeTruthy();
   });
 
-
   it('should get user by ID', () => {
  const userId = '1'
- const mockUser = {id:userId, name: 'John Doe'};
+ const mockUser = {
+  id:userId,
+  email: 'johndoe@gmail.com',
+  lastName: 'John',
+  firstName: 'Doe',
+  admin: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
 expect(service.getById(userId).subscribe(user => {
   expect(user).toEqual(mockUser);
   }))
+
+  const req = httpTesting.expectOne(`${path}/${userId}`);
+  expect(req.request.method).toEqual('GET');
+  req.flush(mockUser)
+
 })
 
 it('should get user is not found', ()  => {
   const userId = '10'
-  
   expect(service.getById(userId).subscribe(user => {
-
     expect(user).toBe(404);
-   
   }))
+  const req = httpTesting.expectOne(`${path}/${userId}`);
+  expect(req.request.method).toEqual('GET');
+  req.flush('user is not found');
+
+})
+
+it('should delete user by id', () => {
+  const userId = '1'
+
+  expect(service.delete(userId).subscribe(res => {
+    expect(res).toBeNull()
+  }))
+
+  const req = httpTesting.expectOne(`${path}/${userId}`);
+  expect(req.request.method).toEqual('DELETE');
+  req.flush(null);
+
 })
 
 });
