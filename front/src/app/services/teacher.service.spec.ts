255702fa67bc53c67f28efdfe68acff6b5b2ece6
Alexandre Mazoyer (EXT) <amazoyer@cegid.com>
2024-01-24 17:37:44 +0100

    add test teacher session

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -3,20 +3,37 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpTesting:HttpTestingController
+  const path = 'api/user';
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports:[ HttpClientModule],
+      providers: [TeacherService]
     });
     service = TestBed.inject(TeacherService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get All Teacher', () => {
+
+ const mockUser = {
+  id:,
+  email: 'johndoe@gmail.com',
+  lastName: 'John',
+  firstName: 'Doe',
+  admin: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
 });
