192137c8dcb4a5188b35f287eca825ed49a3ae19
Mazoyer <mazoyeralexandre@hotmail.fr>
2024-01-26 16:36:37 +0100

    add test Junit

---
+++ back/src/test/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -0,0 +1,150 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class SessionServiceTest {
+
+
+    private SessionService sessionService;
+
+    @Mock
+    private SessionRepository sessionRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+
+    @Test
+    void create() {
+
+        // on cree un objet session
+        Session session = new Session();
+        // on lui donne un id
+        session.setId(1L);
+        // le when permet de dire que lorsque l'on appelle la méthode save de sessionRepository avec n'importe quel objet session en paramètre, on renvoie l'objet session
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+        // on instancie un objet sessionService avec les mocks sessionRepository et userRepository
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        // on appelle la méthode create de sessionService avec l'objet session en paramètre
+        Session createdSession = sessionService.create(session);
+        // on vérifie que l'objet session retourné par la méthode create de sessionService est bien le même que l'objet session que l'on a créé
+        Assertions.assertEquals(session.getId(), createdSession.getId());
+
+    }
+
+    @Test
+    void delete() {
+
+
+        Session session = new Session();
+        session.setId(1L);
+        // le doNothing permet de dire que lorsque l'on appelle la méthode deleteById de sessionRepository avec l'id 1 en paramètre, on ne fait rien (on ne renvoie rien)
+        doNothing().when(sessionRepository).deleteById(1L);
+        //
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        // on appelle la méthode delete de sessionService avec l'id 1 en paramètre
+        sessionService.delete(1L);
+        // on vérifie que la méthode deleteById de sessionRepository a bien été appelée une fois avec l'id 1 en paramètre
+        verify(sessionRepository, times(1)).deleteById(1L);
+
+    }
+
+    @Test
+    void findAll() {
+        // on crée une liste de sessions
+        List<Session> sessions = new ArrayList<>();
+        // on crée une session
+        Session session = new Session();
+        // on lui donne un id
+        session.setId(1L);
+        // on ajoute la session à la liste de sessions
+        sessions.add(session);
+        // le when permet de dire que lorsque l'on appelle la méthode findAll de sessionRepository, on renvoie la liste de sessions
+        when(sessionRepository.findAll()).thenReturn(sessions);
+        // on instancie un objet sessionService avec les mocks sessionRepository et userRepository
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        // on appelle la méthode findAll de sessionService
+        List<Session> foundSessions = sessionService.findAll();
+        // on vérifie que la liste de sessions retournée par la méthode findAll de sessionService est bien la même que la liste de sessions que l'on a créée
+        assertThat(foundSessions).hasSize(1);
+        // on vérifie que la session retournée par la méthode findAll de sessionService est bien la même que la session que l'on a créée
+        assertThat(foundSessions.get(0).getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    void getById() {
+        Session session = new Session();
+        session.setId(1L);
+        when(sessionRepository.findById(1L)).thenReturn(java.util.Optional.of(session));
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        Session foundSession = sessionService.getById(1L);
+        assertThat(foundSession.getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    void update() {
+
+        Session session = new Session();
+        session.setId(1L);
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        Session updatedSession = sessionService.update(1L, session);
+        assertThat(updatedSession.getId()).isEqualTo(1L);
+    }
+
+  /*    @Test
+    void participate() {
+      Session session = new Session();
+        session.setId(1L);
+
+        User user = new User();
+        user.setId(1L);
+
+        when(sessionRepository.findById(1L)).thenReturn(Optional.of(session));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        SessionService sessionService = new SessionService(sessionRepository, userRepository);
+        sessionService.participate(1L, 1L);
+
+        assertThat(session.getUsers()).contains(user);
+
+        // Verify that the findById method on sessionRepository was called once with the correct argument
+        verify(sessionRepository, times(1)).findById(1L);
+
+        // Verify that the findById method on userRepository was called once with the correct argument
+        verify(userRepository, times(1)).findById(1L);
+    } */
+
+
+    @Test
+    void noLongerParticipate() {
+
+
+
+    }
+}
---
+++ back/src/test/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+
+
+    @Test
+    void delete() {
+        User user = new User();
+        user.setId(1L);
+        doNothing().when(userRepository).deleteById(1L);
+        UserService userService = new UserService(userRepository);
+        userService.delete(1L);
+        verify(userRepository, times(1)).deleteById(1L);
+
+    }
+
+    @Test
+    void findById() {
+        User user = new User();
+        user.setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        UserService userService = new UserService(userRepository);
+        User foundUser = userService.findById(1L);
+        assertThat(foundUser.getId()).isEqualTo(1L);
+    }
+}
